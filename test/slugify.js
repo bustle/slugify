0507b7272d989e8ec4f0a67cedc43eed7c50e6f4
Michael Hart <michael.hart.au@gmail.com>
2020-10-06 09:45:21 -0400

    Update deps

--- test/slugify.js
+++ test/slugify.js
@@ -10,49 +10,49 @@ const slugPost = slugify.slugPost
 describe('slugify', () => {
   describe('#slug', () => {
     it('should slugify a string', () => {
-      assert.equal(slug('a generic string'), 'a-generic-string')
+      assert.strictEqual(slug('a generic string'), 'a-generic-string')
     })
 
     it('should not expand certain chars', () => {
-      assert.equal(slug('Bet $5k on the Knicks'), 'bet-5k-on-the-knicks')
+      assert.strictEqual(slug('Bet $5k on the Knicks'), 'bet-5k-on-the-knicks')
     })
   })
 
   describe('#slugUser', () => {
     it('should put the id at the end', () => {
-      assert.equal(slugUser({ id: 1, name: 'Penny Dog' }), 'penny-dog-1')
+      assert.strictEqual(slugUser({ id: 1, name: 'Penny Dog' }), 'penny-dog-1')
     })
 
     it('should ignore a missing name', () => {
-      assert.equal(slugUser({ id: 2 }), '2')
+      assert.strictEqual(slugUser({ id: 2 }), '2')
     })
   })
 
   describe('#slugPost', () => {
     it('should put the id at the end', () => {
-      assert.equal(slugPost({ id: 1, title: 'My Great Post!' }), 'my-great-post-1')
+      assert.strictEqual(slugPost({ id: 1, title: 'My Great Post!' }), 'my-great-post-1')
     })
 
     it('should remove stop words', () => {
-      assert.equal(slugPost({ id: 1, title: 'This and That!' }), 'this-that-1')
+      assert.strictEqual(slugPost({ id: 1, title: 'This and That!' }), 'this-that-1')
     })
 
     it('should ignore a missing title', () => {
-      assert.equal(slugPost({ id: 2 }), '2')
+      assert.strictEqual(slugPost({ id: 2 }), '2')
     })
   })
 
   describe('#slugArticle', () => {
     it('should put the id at the beginning', () => {
-      assert.equal(slugArticle({ id: 1, title: 'My Great Post!' }), '1-my-great-post')
+      assert.strictEqual(slugArticle({ id: 1, title: 'My Great Post!' }), '1-my-great-post')
     })
 
     it('should remove stop words', () => {
-      assert.equal(slugArticle({ id: 1, title: 'This and That!' }), '1-this-that')
+      assert.strictEqual(slugArticle({ id: 1, title: 'This and That!' }), '1-this-that')
     })
 
     it('should ignore a missing title', () => {
-      assert.equal(slugArticle({ id: 2 }), '2')
+      assert.strictEqual(slugArticle({ id: 2 }), '2')
     })
   })
 })
